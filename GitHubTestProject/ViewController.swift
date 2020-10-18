a47b0268d78d53a2b6dce8a8257685e1da279593
Leo Kayembe <leokays@gmail.com>
2020-10-18 16:49:49 -0400

    Initial Commit

---
+++ GitHubTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubTestProject
+//
+//  Created by Kayembe Leo on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
